3caa9c9758bcab19644a79062b43665fd915cdd9
hyfly233 <hyfly233@outlook.com>
2020-01-02 12:00:00 +0800

    commit

---
+++ src/py_faiss/config.py
@@ -0,0 +1,44 @@
+from pydantic_settings import BaseSettings
+from typing import List
+import os
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Document Search API"
+    VERSION: str = "0.1.0"
+    API_V1_STR: str = "/api/v1"
+
+    # Ollama 配置
+    OLLAMA_BASE_URL: str = "http://localhost:11434"
+    EMBEDDING_MODEL: str = "bge-m3"
+    EMBEDDING_DIMENSION: int = 1024
+
+    # 存储配置
+    STORAGE_PATH: str = "./storage"
+    INDEX_PATH: str = "./storage/indexes"
+    DOCUMENTS_PATH: str = "./storage/documents"
+    TEMP_PATH: str = "./storage/temp"
+
+    # 文档处理配置
+    MAX_FILE_SIZE: int = 50 * 1024 * 1024  # 50MB
+    CHUNK_SIZE: int = 512
+    CHUNK_OVERLAP: int = 50
+
+    # API 配置
+    ALLOWED_HOSTS: List[str] = ["*"]
+    MAX_SEARCH_RESULTS: int = 20
+
+    # 日志配置
+    LOG_LEVEL: str = "INFO"
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+settings = Settings()
+
+# 确保存储目录存在
+for path in [settings.STORAGE_PATH, settings.INDEX_PATH,
+             settings.DOCUMENTS_PATH, settings.TEMP_PATH]:
+    os.makedirs(path, exist_ok=True)
--- src/py_faiss/main.py
+++ src/py_faiss/main.py
@@ -1,2 +1,22 @@
+import os
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+load_dotenv()
+
+
+app = FastAPI(title=)
+
+
 def main() -> None:
-    print("Hello from py-faiss!")
+    app_host = os.getenv('APP_HOST', '0.0.0.0')
+    app_port = os.getenv('APP_PORT', 8080)
+
+    uvicorn.run(
+        "py_faiss.main:app",
+        host=app_host,
+        port=app_port,
+        reload=True
+    )
