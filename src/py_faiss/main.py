dae8ac0bee5f4f430e0ae10b9c991bf08477d899
hyfly233 <hyfly233@outlook.com>
2020-01-07 12:00:00 +0800

    commit

--- src/py_faiss/main.py
+++ src/py_faiss/main.py
@@ -87,7 +87,7 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 
 # 包含路由
-# app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 @app.get("/")
