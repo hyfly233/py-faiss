d6c479958b5cbb9d8dc74094026855a819a98451
hyfly233 <hyfly233@outlook.com>
2020-01-24 12:00:00 +0800

    commit

--- src/py_faiss/core/embedding.py
+++ src/py_faiss/core/embedding.py
@@ -1,10 +1,23 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from py_faiss.config import settings
 
+
 class EmbeddingService():
-    def __init__(self):
-        self.base_url = settings.OLLAMA_BASE_URL
-        self.model = settings.EMBEDDING_MODEL
-        self.embedding_dim = settings.EMBEDDING_DIMENSION
+    def __init__(self, base_url: str = None, model_name: str = None, dimension: int = None, timeout: int = 30,
+                 max_retries: int = 3):
+        self.base_url = base_url or settings.OLLAMA_BASE_URL
+        self.model_name = model_name or settings.EMBEDDING_MODEL
+        self.dimension = dimension or settings.EMBEDDING_DIMENSION
+        self.timeout = timeout
+        self.max_retries = max_retries
+
+        self.embeddings_url = f"{self.base_url}/api/embeddings"
+        self.tags_url = f"{self.base_url}/api/tags"
+
+        # 连接池配置
+        self.session = None
+        self._executor = ThreadPoolExecutor(max_workers=4)
 
     async def initialize(self):
         pass
