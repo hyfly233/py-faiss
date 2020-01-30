30d70295628cc0306b3aec45bc2114e0c7205366
hyfly233 <hyfly233@outlook.com>
2020-01-30 12:00:00 +0800

    commit

--- src/py_faiss/core/document_processor.py
+++ src/py_faiss/core/document_processor.py
@@ -1,5 +1,5 @@
 
-class DocumentProcessor():
+class DocumentProcessor:
     def __init__(self):
         self.embedding_service = None  # Placeholder for the embedding service
         self.chunk_size = 512  # Default chunk size
--- src/py_faiss/core/embedding.py
+++ src/py_faiss/core/embedding.py
@@ -1,12 +1,16 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor
-import asyncio
+from typing import List
+
 import aiohttp
+import numpy as np
+
 from py_faiss.config import settings
 
 logger = logging.getLogger(__name__)
 
-class EmbeddingService():
+
+class EmbeddingService:
     def __init__(self, base_url: str = None, model_name: str = None, dimension: int = None, timeout: int = 30,
                  max_retries: int = 3):
         self.base_url = base_url or settings.OLLAMA_BASE_URL
@@ -24,11 +28,45 @@ class EmbeddingService():
 
     async def initialize(self):
         try:
+            # 创建 aiohttp 会话
             self.session = aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=self.timeout)
             )
+
+            # 检查 Ollama 连接
+            await self._check_ollama_connection()
+
+            # 验证模型可用性
+            await self._verify_model()
+
+            # 测试 embedding 生成
+            await self._test_embedding()
+
+            logger.info(f"Embedding service initialized with model: {self.model_name}")
+            return True
+
         except Exception as e:
             logger.error(f"Failed to initialize embedding service: {e}")
             if self.session:
                 await self.session.close()
                 self.session = None
             raise
+
+    async def _check_ollama_connection(self):
+        pass
+
+    async def _verify_model(self):
+        pass
+
+    async def _test_embedding(self):
+        pass
+
+    async def get_embedding(self, text: str) -> np.ndarray:
+        pass
+
+    async def get_embeddings_batch(self, texts: List[str], batch_size: int = 10,
+                                   show_progress: bool = True) -> np.ndarray:
+        pass
+
+    async def _process_batch_concurrent(self, texts: List[str]) -> List[np.ndarray]:
+        pass
