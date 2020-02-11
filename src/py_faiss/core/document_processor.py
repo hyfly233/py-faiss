8a84a8dff2da24accace603148f34c7e836bdd62
hyfly233 <hyfly233@outlook.com>
2020-02-11 12:00:00 +0800

    commit

--- src/py_faiss/core/document_processor.py
+++ src/py_faiss/core/document_processor.py
@@ -1,6 +1,21 @@
+import logging
+
+from py_faiss.config import settings
+
+logger = logging.getLogger(__name__)
 
 class DocumentProcessor:
+    """文档处理器 - 支持多种文档格式的文本提取和处理"""
+
     def __init__(self):
-        self.embedding_service = None  # Placeholder for the embedding service
-        self.chunk_size = 512  # Default chunk size
-        self.overlap = 50  # Default overlap size
+        self.supported_extensions = {
+            '.docx', '.doc',  # Word 文档
+            '.pdf',  # PDF 文档
+            '.txt', '.md',  # 文本文档
+            '.xlsx', '.xls',  # Excel 文档
+            '.csv',  # CSV 文档
+            '.json',  # JSON 文档
+            '.xml',  # XML 文档
+        }
+
+
