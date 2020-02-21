b7717ee9045fdacfe6a97a00f59cc9d76010a549
hyfly233 <hyfly233@outlook.com>
2020-02-21 12:00:00 +0800

    commit

--- src/py_faiss/core/document_processor.py
+++ src/py_faiss/core/document_processor.py
@@ -48,3 +48,34 @@ class DocumentProcessor:
         if file_size > settings.MAX_FILE_SIZE:
             raise ValueError(f"文件过大: {file_size / 1024 / 1024:.1f}MB > {settings.MAX_FILE_SIZE / 1024 / 1024}MB")
 
+        # 获取文件扩展名
+        extension = file_path.suffix.lower()
+
+        if extension not in self.supported_extensions:
+            raise ValueError(f"不支持的文件格式: {extension}")
+
+        try:
+            # 根据文件类型选择提取方法
+            if extension in ['.docx', '.doc']:
+                text = await self._extract_from_docx(file_path)
+            elif extension == '.pdf':
+                text = await self._extract_from_pdf(file_path)
+            elif extension in ['.txt', '.md']:
+                text = await self._extract_from_text(file_path)
+            elif extension in ['.xlsx', '.xls']:
+                text = await self._extract_from_excel(file_path)
+            elif extension == '.csv':
+                text = await self._extract_from_csv(file_path)
+            elif extension == '.json':
+                text = await self._extract_from_json(file_path)
+            elif extension == '.xml':
+                text = await self._extract_from_xml(file_path)
+            else:
+                raise ValueError(f"暂不支持的文件格式: {extension}")
+
+            logger.info(f"成功提取文本: {file_path.name}, 长度: {len(text)} 字符")
+            return text
+
+        except Exception as e:
+            logger.error(f"文本提取失败 {file_path.name}: {e}")
+            raise
