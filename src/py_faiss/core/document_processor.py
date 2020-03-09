56769a885d7c712704655dcdce3f823bba4bb71a
hyfly233 <hyfly233@outlook.com>
2020-03-09 12:00:00 +0800

    commit

--- src/py_faiss/core/document_processor.py
+++ src/py_faiss/core/document_processor.py
@@ -235,7 +235,41 @@ class DocumentProcessor:
             raise Exception(f"Excel 处理失败: {e}")
 
     async def _extract_from_csv(self, file_path: Path) -> str:
-        pass
+        """从 CSV 文件提取文本"""
+        try:
+            loop = asyncio.get_event_loop()
+
+            def _read_csv():
+                # 尝试不同编码
+                for encoding in ['utf-8', 'gbk', 'gb2312', 'latin-1']:
+                    try:
+                        df = pd.read_csv(file_path, encoding=encoding)
+                        break
+                    except:
+                        continue
+                else:
+                    raise Exception("无法识别 CSV 文件编码")
+
+                content = []
+
+                # 添加列标题
+                headers = ' | '.join(str(col) for col in df.columns)
+                content.append(headers)
+
+                # 添加数据行
+                for _, row in df.iterrows():
+                    row_data = [str(val) if pd.notna(val) else '' for val in row]
+                    row_text = ' | '.join(filter(None, row_data))
+                    if row_text.strip():
+                        content.append(row_text)
+
+                return '\n'.join(content)
+
+            text = await loop.run_in_executor(None, _read_csv)
+            return text
+
+        except Exception as e:
+            raise Exception(f"CSV 处理失败: {e}")
 
     async def _extract_from_json(self, file_path: Path) -> str:
         pass
