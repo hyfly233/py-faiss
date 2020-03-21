795185e8eb2a0dc36826312623abecd2dd6f0df9
hyfly233 <hyfly233@outlook.com>
2020-03-21 12:00:00 +0800

    commit

--- src/py_faiss/api/v1/endpoints/search.py
+++ src/py_faiss/api/v1/endpoints/search.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from py_faiss.core.search_engine import SearchEngine
+from py_faiss.dependencies import get_search_engine
+from py_faiss.models.requests import SearchRequest, SearchResponse
 
 router = APIRouter()
 
 
-@router.get("/test")
-async def test():
-    return [{"test": "search"}]
+@router.post("/search", response_model=SearchResponse)
+async def search_documents(request: SearchRequest, search_engine: SearchEngine = Depends(get_search_engine)):
+    pass
+
+
+@router.get("/search/stats")
+async def get_search_stats(search_engine: SearchEngine = Depends(get_search_engine)):
+    pass
