deb793986bb6b2f5504a925e8978081572b15cde
hyfly233 <hyfly233@outlook.com>
2020-05-08 12:00:00 +0800

    commit

--- src/py_faiss/services/document_service.py
+++ src/py_faiss/services/document_service.py
@@ -103,4 +103,93 @@ class DocumentService:
         file_path: Path,
         metadata: Optional[Dict[str, Any]] = None
     ):
-        pass
+        """异步处理文档"""
+        try:
+            # 更新状态：文本提取
+            self._update_processing_status(doc_id, 10, "正在提取文档文本...")
+
+            # 处理文档
+            process_result = await self.document_processor.process_document(file_path)
+
+            if process_result['status'] != 'success':
+                raise Exception(f"文档处理失败: {process_result.get('error', 'Unknown error')}")
+
+            chunks = process_result['chunks']
+
+            # 更新状态：生成嵌入向量
+            self._update_processing_status(doc_id, 30, f"正在生成嵌入向量... ({len(chunks)} 个文本块)")
+
+            # 生成嵌入向量
+            embeddings = await self.embedding_service.get_embeddings_batch(
+                chunks,
+                show_progress=False
+            )
+
+            # 更新状态：创建文档对象
+            self._update_processing_status(doc_id, 70, "正在创建文档索引...")
+
+            # 创建文档对象
+            documents = []
+            file_hash = hashlib.md5(str(file_path).encode()).hexdigest()
+
+            for i, chunk in enumerate(chunks):
+                doc_metadata = {
+                    'file_size': process_result['file_size'],
+                    'processing_time': process_result['processing_time'],
+                    'document_hash': process_result['document_hash'],
+                    'file_hash': file_hash,
+                    'chunk_length': len(chunk),
+                    **(metadata or {})
+                }
+
+                doc = Document(
+                    doc_id=doc_id,
+                    file_path=str(file_path),
+                    file_name=process_result['file_name'],
+                    chunk_index=i,
+                    text=chunk,
+                    metadata=doc_metadata
+                )
+                documents.append(doc)
+
+            # 更新状态：添加到向量存储
+            self._update_processing_status(doc_id, 90, "正在添加到向量数据库...")
+
+            # 添加到向量存储
+            success = await self.vector_store.add_documents(documents, embeddings)
+
+            if not success:
+                raise Exception("添加到向量存储失败")
+
+            # 完成处理
+            self.processing_status[doc_id] = {
+                'status': 'completed',
+                'filename': process_result['file_name'],
+                'started_at': self.processing_status[doc_id]['started_at'],
+                'completed_at': datetime.now().isoformat(),
+                'progress': 100,
+                'message': '文档处理完成',
+                'chunks_count': len(chunks),
+                'file_size': process_result['file_size'],
+                'document_hash': process_result['document_hash']
+            }
+
+            logger.info(f"文档处理完成: {doc_id} ({process_result['file_name']})")
+
+        except Exception as e:
+            logger.error(f"文档处理失败 {doc_id}: {e}")
+            self.processing_status[doc_id] = {
+                'status': 'error',
+                'error': str(e),
+                'filename': self.processing_status[doc_id].get('filename', 'Unknown'),
+                'started_at': self.processing_status[doc_id]['started_at'],
+                'failed_at': datetime.now().isoformat(),
+                'message': f'文档处理失败: {str(e)}'
+            }
+        finally:
+            # 清理临时文件
+            try:
+                if file_path.exists():
+                    file_path.unlink()
+            except Exception as e:
+                logger.warning(f"清理临时文件失败: {e}")
