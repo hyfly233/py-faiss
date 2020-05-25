3948b78a075907139a4b4298e5991e492ae76b14
hyfly233 <hyfly233@outlook.com>
2020-05-25 12:00:00 +0800

    commit

--- src/py_faiss/api/v1/endpoints/documents.py
+++ src/py_faiss/api/v1/endpoints/documents.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, HTTPException, Query, Depends
+from typing import List, Optional
+import logging
+
+from py_faiss.services.document_service import get_document_service
 
 router = APIRouter()
+
+logger = logging.getLogger(__name__)
 
 
 @router.get("/test")
