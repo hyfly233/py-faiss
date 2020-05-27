e61ebfdce3823b28840d1f476144ee9d4f438f25
hyfly233 <hyfly233@outlook.com>
2020-05-27 12:00:00 +0800

    commit

--- src/py_faiss/api/v1/endpoints/documents.py
+++ src/py_faiss/api/v1/endpoints/documents.py
@@ -8,3 +8,35 @@ router = APIRouter()
 
 logger = logging.getLogger(__name__)
 
+
+@router.post("/upload")
+async def upload_document(
+        file: UploadFile = File(...),
+        user_id: Optional[str] = None
+):
+    """上传文档"""
+    try:
+        # 检查文件类型
+        document_service = await get_document_service()
+
+        if not document_service.document_processor.is_supported_file(file.filename):
+            raise HTTPException(
+                status_code=400,
+                detail=f"不支持的文件类型: {file.filename}"
+            )
+
+        # 读取文件内容
+        content = await file.read()
+
+        # 处理文档
+        result = await document_service.upload_and_process_document(
+            file_content=content,
+            filename=file.filename,
+            user_id=user_id
+        )
+
+        return result
+
+    except Exception as e:
+        logger.error(f"上传文档失败: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
