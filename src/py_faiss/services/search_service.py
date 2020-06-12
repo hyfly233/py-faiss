5513e937e702d1a7f8e179d690d331fb47b476af
hyfly233 <hyfly233@outlook.com>
2020-06-12 12:00:00 +0800

    commit

--- src/py_faiss/services/search_service.py
+++ src/py_faiss/services/search_service.py
@@ -13,3 +13,21 @@ from py_faiss.services.document_service import get_document_service
 from py_faiss.config import settings
 
 logger = logging.getLogger(__name__)
+
+class SearchFilter:
+    """搜索过滤器"""
+    def __init__(
+        self,
+        doc_ids: Optional[List[str]] = None,
+        file_names: Optional[List[str]] = None,
+        file_types: Optional[List[str]] = None,
+        date_range: Optional[Tuple[str, str]] = None,
+        min_score: float = 0.0,
+        metadata_filters: Optional[Dict[str, Any]] = None
+    ):
+        self.doc_ids = doc_ids
+        self.file_names = file_names
+        self.file_types = file_types
+        self.date_range = date_range
+        self.min_score = min_score
+        self.metadata_filters = metadata_filters or {}
