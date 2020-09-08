6d300509acfc2262f639827bb0135d7747054553
hyfly233 <hyfly233@outlook.com>
2020-09-08 12:00:00 +0800

    commit

--- src/py_faiss/api/v1/endpoints/health.py
+++ src/py_faiss/api/v1/endpoints/health.py
@@ -1,20 +1,20 @@
 import asyncio
 import logging
-import psutil
+import os
 import time
-from typing import Dict, Any, List, Optional
 from datetime import datetime, timedelta
 from pathlib import Path
-import json
+from typing import Dict, Any, List, Optional
 
-from fastapi import APIRouter, HTTPException, Depends
+import psutil
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
+from py_faiss.config import settings
 from py_faiss.core.embedding import get_embedding_service
 from py_faiss.core.vector_store import get_vector_store
 from py_faiss.services.document_service import get_document_service
 from py_faiss.services.search_service import get_search_service
-from py_faiss.config import settings
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
