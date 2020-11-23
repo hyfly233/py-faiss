9e33da8cb3e57ad23a8d9696bcdd1fd0b1b6e314
hyfly233 <hyfly233@outlook.com>
2020-11-23 12:00:00 +0800

    commit

--- tests/test_document_processor.py
+++ tests/test_document_processor.py
@@ -8,3 +8,36 @@ import aiofiles
 
 from py_faiss.core.document_processor import DocumentProcessor, document_processor
 from py_faiss.config import settings
+
+
+class TestDocumentProcessor:
+    """文档处理器测试类"""
+
+    @pytest.fixture
+    def processor(self):
+        """创建文档处理器实例"""
+        return DocumentProcessor()
+
+    @pytest.fixture
+    def temp_dir(self):
+        """创建临时目录"""
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            yield Path(tmp_dir)
+
+    @pytest.fixture
+    def sample_texts(self):
+        """示例文本内容"""
+        return {
+            'simple': "这是一个简单的测试文档。",
+            'long': "这是一个很长的文档。" + "测试内容。" * 100,
+            'multi_paragraph': """第一段内容。
+
+第二段内容，包含更多信息。
+
+第三段内容。""",
+            'unicode': "这是包含中文、English和éñglish的文档。",
+            'empty': "",
+            'whitespace': "   \n\t   \n   "
+        }
+
+    # ========== 基础功能测试 ==========
