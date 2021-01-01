d711d99ac4a39d4bb26c4604c5fac042bd681cb9
hyfly233 <hyfly233@outlook.com>
2021-01-01 12:00:00 +0800

    commit

--- tests/test_document_processor.py
+++ tests/test_document_processor.py
@@ -1,13 +1,12 @@
-import pytest
 import asyncio
 import tempfile
-import os
 from pathlib import Path
-from unittest.mock import patch, MagicMock, AsyncMock
+from unittest.mock import patch, MagicMock
+
 import aiofiles
+import pytest
 
 from py_faiss.core.document_processor import DocumentProcessor, document_processor
-from py_faiss.config import settings
 
 
 class TestDocumentProcessor:
