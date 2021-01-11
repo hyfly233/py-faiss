8f9332b8da8d9a7183f2c7bb2b9e8755dfea0b8e
hyfly233 <hyfly233@outlook.com>
2021-01-11 12:00:00 +0800

    commit

--- tests/test_document_processor_extract.py
+++ tests/test_document_processor_extract.py
@@ -1,12 +1,11 @@
-import asyncio
 import tempfile
 from pathlib import Path
-from unittest.mock import patch, MagicMock
 
 import aiofiles
 import pytest
 
-from py_faiss.core.document_processor import DocumentProcessor, document_processor
+from py_faiss.core.document_processor import DocumentProcessor
+
 
 class TestDocumentProcessor:
     """文档处理器测试类"""
@@ -41,7 +40,8 @@ class TestDocumentProcessor:
     @pytest.mark.asyncio
     async def test_save_temp_file(self, processor, temp_dir):
         """测试保存临时文件"""
-        content = f"测试文件内容"
+        # 修复：确保content是bytes类型
+        content = "测试文件内容".encode('utf-8')  # 转换为bytes
         filename = "test.txt"
 
         # 临时修改处理器的临时目录
@@ -62,3 +62,102 @@ class TestDocumentProcessor:
 
         finally:
             processor.temp_dir = original_temp_dir
+
+    @pytest.mark.asyncio
+    async def test_save_temp_file_text_mode(self, processor, temp_dir):
+        """测试保存临时文件（文本模式验证）"""
+        content_str = "测试文件内容"
+        content_bytes = content_str.encode('utf-8')
+        filename = "test.txt"
+
+        original_temp_dir = processor.temp_dir
+        processor.temp_dir = temp_dir
+
+        try:
+            file_path = await processor.save_temp_file(content_bytes, filename)
+
+            assert file_path.exists()
+            assert file_path.name.endswith("_test.txt")
+            assert file_path.parent == temp_dir
+
+            # 验证文件内容（文本模式读取）
+            async with aiofiles.open(file_path, 'r', encoding='utf-8') as f:
+                saved_content = await f.read()
+                assert saved_content == content_str
+
+        finally:
+            processor.temp_dir = original_temp_dir
+
+    def test_get_supported_types(self, processor):
+        """测试获取支持的文件类型"""
+        supported_types = processor.get_supported_types()
+
+        assert isinstance(supported_types, list)
+        assert len(supported_types) > 0
+        assert 'txt' in supported_types
+
+    def test_is_supported_file(self, processor):
+        """测试文件类型支持检查"""
+        # 支持的文件类型
+        assert processor.is_supported_file("test.txt") == True
+
+        # 不支持的文件类型
+        assert processor.is_supported_file("image.jpg") == False
+        assert processor.is_supported_file("video.mp4") == False
+
+        # 边界情况
+        assert processor.is_supported_file("") == False
+        assert processor.is_supported_file("noextension") == False
+
+    def test_chunk_text(self, processor, sample_texts):
+        """测试文本分块"""
+        # 短文本
+        chunks = processor._chunk_text(sample_texts['simple'])
+        assert len(chunks) >= 1
+
+        # 长文本
+        chunks = processor._chunk_text(sample_texts['long'])
+        assert len(chunks) >= 1
+
+        # 验证每个块的长度不超过限制
+        for chunk in chunks:
+            assert len(chunk) <= processor.chunk_size
+
+        # 空文本
+        chunks = processor._chunk_text(sample_texts['empty'])
+        assert len(chunks) == 0
+
+    def create_temp_txt_file(self, temp_dir: Path, content: str, filename: str = "test.txt") -> Path:
+        """创建临时TXT文件"""
+        file_path = temp_dir / filename
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(content)
+        return file_path
+
+    @pytest.mark.asyncio
+    async def test_process_txt_file(self, processor, temp_dir, sample_texts):
+        """测试TXT文件处理"""
+        file_path = self.create_temp_txt_file(temp_dir, sample_texts['multi_paragraph'])
+
+        result = await processor._process_txt_file(file_path)
+
+        assert result['status'] == 'success'
+        assert len(result['chunks']) >= 1
+        assert result['file_name'] == file_path.name
+        assert result['file_size'] > 0
+        assert 'processing_time' in result
+        assert 'document_hash' in result
+
+    @pytest.mark.asyncio
+    async def test_process_document(self, processor, temp_dir, sample_texts):
+        """测试主文档处理方法"""
+        file_path = self.create_temp_txt_file(temp_dir, sample_texts['multi_paragraph'])
+
+        result = await processor.process_document(file_path)
+
+        assert result['status'] == 'success'
+        assert len(result['chunks']) >= 1
+        assert result['file_name'] == file_path.name
+        assert result['file_size'] > 0
+        assert result['processing_time'] > 0
+        assert len(result['document_hash']) == 64  # SHA256 hash length
