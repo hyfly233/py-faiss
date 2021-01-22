2e5ac1a615f465367e18c15d3eca2789119c19c8
hyfly233 <hyfly233@outlook.com>
2021-01-22 12:00:00 +0800

    commit

--- src/py_faiss/core/faiss_persistence.py
+++ src/py_faiss/core/faiss_persistence.py
@@ -1,3 +1,15 @@
 import faiss
-import numpy as np
+import pickle
+import json
+import os
+from typing import Dict, List, Any
+from datetime import datetime
 
+class FAISSPersistence:
+    def __init__(self, base_path: str):
+        self.base_path = base_path
+        self.index_file = os.path.join(base_path, "faiss_index.bin")
+        self.metadata_file = os.path.join(base_path, "metadata.pkl")
+        self.config_file = os.path.join(base_path, "config.json")
+
+        os.makedirs(base_path, exist_ok=True)
