66a33eea015f896dfe184794e07dfcb5b78a6dec
hyfly233 <hyfly233@outlook.com>
2023-11-02 12:00:00 +0800

    commit

--- src/py_faiss/services/document_service.py
+++ src/py_faiss/services/document_service.py
@@ -3,9 +3,9 @@ import hashlib
 import logging
 import uuid
 from datetime import datetime
-from pathlib import Path
 from typing import List, Dict, Any, Optional
 
+from pathlib import Path
 from py_faiss.core.document_processor import document_processor
 from py_faiss.core.embedding import get_embedding_service
 from py_faiss.core.vector_store import get_vector_store, Document
